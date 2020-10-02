c53cb3b147b885e85d582affb9dc0e4f12554fd6
Andrew <healeycodes@gmail.com>
2020-10-02 17:56:05 +0100

    Add skeleton of schema

---
+++ indexer.py
@@ -0,0 +1,35 @@
+from sqlalchemy import create_engine
+from sqlalchemy import Column, Integer, ForeignKey, String, DateTime
+from sqlalchemy.orm import relationship
+from sqlalchemy.sql import func
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class Page(Base):
+    __tablename__ = 'page'
+    created_at = Column(DateTime(timezone=True), default=func.now())
+    updated_at = Column(DateTime(timezone=True), onupdate=func.now())
+    id = Column(Integer, primary_key=True)
+    url = Column(String())
+    title = Column(String())
+    locations = relationship("Location")
+
+
+class Location(Base):
+    __tablename__ = 'location'
+    id = Column(Integer, primary_key=True)
+    position = Column(String())
+    word_id = Column(Integer, ForeignKey('word.id'))
+    page_id = Column(Integer, ForeignKey('page.id'))
+
+
+class Word(Base):
+    __tablename__ = 'word'
+    id = Column(Integer, primary_key=True)
+    stem = Column(String())
+    locations = relationship("Location")
+
+
+engine = create_engine('sqlite:///db.sqlite', echo=True)
