9287ce077004215c052318049ba4bd0d804c1025
JEROME Parramore <jerome.parramore@gmail.com>
2020-10-03 14:40:50 +0100

    add pages dict with text body

--- crawler.py
+++ crawler.py
@@ -6,33 +6,31 @@ from urllib.parse import urlparse
 from collections import deque
 import yaml
 
-class Page:
-  def __init__(self, url, text):
-    self.url = url
-    self.text = text
+# class Page:
+#     def __init__(self, url, text):
+#         self.url = url
+#         self.text = text
 
-new_urls = deque([])
 
-# load urls from seed file
-stream = open("seed.yaml", 'r')
-seeds = yaml.safe_load(stream)['seed-urls']
-# queue all seed urls
-[ new_urls.append(url) for url in seeds ]
 
-# processed urls
-unique_urls = set()
+def stream_seeds_into_queue():
 
-# internal urls
-internal_urls = set()
+    new_urls = deque([])
+    stream = open("seed.yaml", 'r')
+    seeds = yaml.safe_load(stream)['seed-urls']
+    [new_urls.append(url) for url in seeds]
 
-# external urls
-external_urls = set()
-
-# broken urls
-broken_urls = set()
+    process_urls(new_urls)
 
 # process urls one by one until we exhaust the queue
-def process_urls():
+
+
+def process_urls(new_urls):
+
+    pages = {}
+    unique_urls = set()
+    broken_urls = set()
+
     while len(new_urls):
         # move url from the queue to processed url set
         url = new_urls.popleft()
@@ -40,30 +38,47 @@ def process_urls():
         print('Processing %s' % url)
         try:
             response = requests.get(url)
-        except(requests.exceptions.MissingSchema, requests.exceptions.ConnectionError, requests.exceptions.InvalidURL, requests.exceptions.InvalidSchema):    
-            # add broken urls to it’s own set, then continue
+        except(requests.exceptions.MissingSchema, requests.exceptions.ConnectionError, requests.exceptions.InvalidURL, requests.exceptions.InvalidSchema):
             broken_urls.add(url)
             continue
 
+        response = requests.get(url)
+        soup = BeautifulSoup(response.text, 'lxml')
+        pages[url] = soup.get_text()
+
         base_obj = extract_base(url)
-        scrape_url_for_links(base_obj, response)
+        internal_urls = scrape_url_for_links(base_obj, soup)
 
         for i in internal_urls:
             if not i in new_urls and not i in unique_urls:
                 new_urls.append(i)
 
-def scrape_url_for_links(base, response):
-    
-    soup = BeautifulSoup(response.text, 'lxml')
-    #print(soup.get_text()) # print the page's text too
+    print(pages)
+    print(unique_urls)
 
-    base_url=base['base_url']
-    strip_base=base['strip_base']
-    path=base['path']
 
-    for link in soup.find_all('a'): 
+def scrape_url_for_links(base, soup):
+
+
+    internal_urls = set()
+    external_urls = set()
+
+    base_url = base['base_url']
+    #print('BASE URL: ' + base_url)
+    strip_base = base['strip_base']
+    #print('STRIP BASE: ' + strip_base)
+    path = base['path']
+    #print('PATH: ' + path)
+
+    for link in soup.find_all('a'):
         # extract link url from the anchor
-        anchor = link.attrs['href'] if 'href' in link.attrs else ''
+
+        if 'href' in link.attrs and not link.attrs['href'].startswith('mailto:'):
+            anchor = link.attrs['href']
+        else:
+            anchor = ''
+
+        #print("ANCHOR: " + anchor)
         if anchor.startswith('/'):
             local_link = base_url + anchor
             internal_urls.add(local_link)
@@ -75,14 +90,23 @@ def scrape_url_for_links(base, response):
         else:
             external_urls.add(anchor)
 
+    return internal_urls
+
 # extract base url to resolve relative links
+
+
 def extract_base(url):
     parts = urlsplit(url)
-    base = '{0.netloc}'.format(parts) # https://docs.python.org/3/library/urllib.parse.html#urllib.parse.urlsplit
-    return {
-            'strip_base' : base.replace('www.', ''),
-            'base_url' : '{0.scheme}://{0.netloc}'.format(parts), 
-            'path' : url[:url.rfind('/')+1] if '/' in parts.path else url
-            }
+    # https://docs.python.org/3/library/urllib.parse.html#urllib.parse.urlsplit
+    base = '{0.netloc}'.format(parts)
 
-process_urls()
+    path = url[:url.rfind('/')+1] if '/' in parts.path else url
+
+    return {
+        'strip_base': base.replace('www.', ''),
+        'base_url': '{0.scheme}://{0.netloc}'.format(parts),
+        'path': path
+    }
+
+stream_seeds_into_queue()
+
