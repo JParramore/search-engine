948f35360a641d0f3620238498af9e3d061f21e8
Andrew <healeycodes@gmail.com>
2020-10-17 15:01:52 +0100

    Add Flask and React skeleton

---
+++ server.py
@@ -0,0 +1,28 @@
+from flask import Flask, request, jsonify, render_template
+from query import query
+app = Flask(__name__,
+            static_url_path='',
+            static_folder='build',
+            template_folder='build')
+
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+
+@app.route('/search')
+def search():
+    q = request.args.get('q') or 'test'
+    return q
+    # return jsonify(query(q))
+
+
+if __name__ == "__main__":
+    '''
+    In development, allow CORS requests so create-react-app
+    can use hot reload and still hit us.
+    '''
+    from flask_cors import CORS
+    CORS(app)
+    app.run(debug=True)
