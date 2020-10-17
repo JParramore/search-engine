cf977ac51c42f11315f43f4f600a9cf7c1b4ee9d
JEROME Parramore <jerome.parramore@gmail.com>
2020-10-17 12:51:58 +0100

    initial query.py commit

--- query.py
+++ query.py
@@ -1,13 +1,21 @@
 from db.services import PageService, LocationService, WordService
 from db.session import get_session
 
-def query(keywords):
+def query(key_words):
+    key_words = key_words.split()
     session = get_session()
     page_service = PageService(session)
     location_service = LocationService(session)
     word_service = WordService(session)
 
-    # pages = []
-    # for key_word in keywords:
-    #     for word in word_service.find(stem=key_word):
-    #         pages.add(word.)
+    # pages_ = []
+    for key_word in key_words:
+        word = word_service.find(stem=key_word)
+        for page in word.pages:
+            
+            num_appearances = len(location_service.find_all(word=word, page=page))
+            print(f'The word "{word.stem}" has appeard {num_appearances} time(s) on {page.url}')
+            
+
+if __name__ == '__main__':
+    query('President')
