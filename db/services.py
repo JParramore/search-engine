e6096719736acaefa3e5af55c4681d681ef4cca4
JEROME Parramore <jerome.parramore@gmail.com>
2020-10-24 19:16:55 +0100

    Location.position => self.model.position in dist between words

--- db/services.py
+++ db/services.py
@@ -66,23 +66,14 @@ class LocationService(ModelServiceBase):
 
     def distance_between_words(self, words, page):
         '''
-<<<<<<< HEAD
         What is the sum of distances between each keyword in order?
         '''
         distance = 0 
         for word, next_word in zip(words, words[1:]):
-            position = self.session.query(self.model).filter_by(page=page, word=word).order_by(Location.position).first().position
-            next_position = self.session.query(self.model).filter_by(page=page, word=next_word).order_by(Location.position).first().position
+            position = self.session.query(self.model).filter_by(page=page, word=word).order_by(self.model.position).first().position
+            next_position = self.session.query(self.model).filter_by(page=page, word=next_word).order_by(self.model.position).first().position
             if position and next_position:
                 distance += abs(position - next_position)
-=======
-        TODO
-        '''
-        distance = sys.maxsize
-
-        # TODO
-
->>>>>>> 7b5e849d79ed202ae5b6cbf7d081f119c59f63b0
         return distance
 
 
